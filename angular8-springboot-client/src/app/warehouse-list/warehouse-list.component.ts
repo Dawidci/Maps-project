fdb69cb28ebbab49532bc1600cf3b2158c0e233b
Dawid Zaleski <dawidci@gazeta.pl>
2019-09-16 15:24:54 +0200

    Map in Warehouse List
    Added map to warehouse list with markets which are those warehouses from list.

--- angular8-springboot-client/src/app/warehouse-list/warehouse-list.component.ts
+++ angular8-springboot-client/src/app/warehouse-list/warehouse-list.component.ts
@@ -3,6 +3,7 @@ import { WarehouseService } from "../warehouse.service";
 import { Warehouse } from "../warehouse";
 import { Component, OnInit } from "@angular/core";
 import { Router } from '@angular/router';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-warehouse-list',
@@ -12,12 +13,31 @@ import { Router } from '@angular/router';
 export class WarehouseListComponent implements OnInit {
 
   warehouses: Observable<Warehouse[]>;
+  map: any;
 
   constructor(private warehouseService: WarehouseService,
               private router: Router) {}
 
   ngOnInit() {
     this.reloadData();
+
+    this.map = L.map('map').setView([30, 0], 2);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    this.warehouses.subscribe(elements => {
+      elements.forEach((warehouse : any) => {
+        console.log(warehouse.name);
+        let marker = new L.Marker([warehouse.latitude, warehouse.longitude]).addTo(this.map);
+        marker.bindPopup("ID: " + warehouse.id.toString() + "<br>" +
+          "Name: " + warehouse.name + "<br>" +
+          "Latitude: " + warehouse.latitude.toString() + "<br>" +
+          "Longitude: " + warehouse.longitude.toString() + "<br>" +
+          "Seaport: " + warehouse.airport.toString() + "<br>" +
+          "Airport: " + warehouse.seaport.toString() + "<br>");
+      })
+    });
   }
 
   reloadData() {
