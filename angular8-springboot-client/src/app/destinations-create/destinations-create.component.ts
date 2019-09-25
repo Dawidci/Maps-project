182974d79e8045c58c26cb1378a620a78ad4e62b
Dawid Zaleski <dawidci@gazeta.pl>
2019-09-25 10:43:37 +0200

    Creating Destinations - works
    Basic creation of destinations now works.
    Next steps:
    - showing details of route - route and list of destinations
    - showing destinations on map
    - algorithm that compute order of destinations

--- angular8-springboot-client/src/app/destinations-create/destinations-create.component.ts
+++ angular8-springboot-client/src/app/destinations-create/destinations-create.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
 import { Route } from '../route';
 import { RouteService } from '../route.service';
 import { RouteListComponent } from '../route-list/route-list.component';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Warehouse } from "../warehouse";
 import { WarehouseService } from "../warehouse.service";
+import { DestinationService } from "../destination.service";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import * as L from 'leaflet';
 import 'leaflet-routing-machine';
+import {Destination} from "../destination";
 
 @Component({
   selector: 'app-destinations-create',
@@ -18,35 +22,109 @@ export class DestinationsCreateComponent implements OnInit {
 
   id: number;
   route0: Route;
+  warehouses: Observable<Warehouse[]>;
+  destinations: Destination[] = [];
   firstWarehouse: Warehouse;
-  firstLatitude: number;
-  firstLongitude: number;
   lastWarehouse: Warehouse;
-  id_first: number;
-  id_last: number;
   name: string;
   map: any;
+  newDestinations: number[] = [];
+  count = 2;
+  submitted = false;
+
+  destinationForm = this.fb.group({
+    firstDestination: [{value: '', disabled: true}, Validators.required],
+    lastDestination: [{value: '', disabled: true}, Validators.required],
+    newDestination: ['', Validators.required]
+  });
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private routeService: RouteService,
-              private warehouseService: WarehouseService) { }
+              private warehouseService: WarehouseService,
+              private destinationService: DestinationService,
+              private fb: FormBuilder) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.route0 = new Route();
+
+    this.reloadData();
+    this.loadMap();
 
     this.id = this.route.snapshot.params['id'];
 
-    this.routeService.getRoute(this.id)
+    await this.routeService.getRoute(this.id)
       .subscribe(data => {
         console.log(data);
         this.route0 = data;
-        this.id_first = this.route0.id_first_warehouse;
-        this.id_last = this.route0.id_last_warehouse;
+        this.destinations.push({id:1, id_route: this.id, id_warehouse: this.route0.id_first_warehouse, order: 1});
+        this.destinations.push({id:2, id_route: this.id, id_warehouse: this.route0.id_last_warehouse, order: 2});
+
+        this.warehouseService.getWarehouse(this.route0.id_first_warehouse)
+          .subscribe(first => {
+            console.log(first);
+            this.firstWarehouse = first;
+          })
+
+        this.warehouseService.getWarehouse(this.route0.id_last_warehouse)
+          .subscribe(last => {
+            console.log(last);
+            this.lastWarehouse = last;
+          })
       }, error => console.log(error));
   }
 
   list(){
     this.router.navigate(['routes/list']);
+  }
+
+  reloadData() {
+    this.warehouses = this.warehouseService.getWarehousesList();
+  }
+
+  loadMap() {
+    this.map = L.map('map').setView([30, 0], 2);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    this.warehouses.subscribe(elements => {
+      elements.forEach((warehouse : any) => {
+        console.log(warehouse.name);
+        let marker = new L.Marker([warehouse.latitude, warehouse.longitude]).addTo(this.map);
+        marker.bindPopup("ID: " + warehouse.id.toString() + "<br>" +
+          "Name: " + warehouse.name + "<br>" +
+          "Latitude: " + warehouse.latitude.toString() + "<br>" +
+          "Longitude: " + warehouse.longitude.toString() + "<br>" +
+          "Seaport: " + warehouse.airport.toString() + "<br>" +
+          "Airport: " + warehouse.seaport.toString() + "<br>");
+      })
+    });
+  }
+
+  addNewDestination() {
+    this.count++;
+    alert("You clicked button!");
+    this.destinations.push({id: this.count, id_route: this.id, id_warehouse: 0, order: this.count});
+    this.newDestinations.push(this.count);
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+    alert('New route and destinations added');
+  }
+
+  async save() {
+    for(let i = 0; i < this.destinations.length; i++) {
+      await this.destinationService.createDestination(this.destinations[i])
+        .subscribe(data => console.log(data), error => console.log(error));
+    }
+
+    this.gotoList();
+  }
+
+  gotoList() {
+    this.router.navigate(['/routes/list']);
   }
 }
--- angular8-springboot-client/src/app/route-create/route-create.component.ts
+++ angular8-springboot-client/src/app/route-create/route-create.component.ts
@@ -7,7 +7,6 @@ import {DestinationService} from "../destination.service";
 import {Warehouse} from "../warehouse";
 import {Router} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Destination} from "../destination";
 
 import * as L from 'leaflet';
 
@@ -21,12 +20,7 @@ export class RouteCreateComponent implements OnInit {
 
   warehouses: Observable<Warehouse[]>;
   route: Route = new Route();
-  id_route: number;
   submitted = false;
-  destinations: Observable<Destination[]>;
-  destination: Destination = new Destination();
-  dest: Destination[] = [];
-  count: number = 0;
   map: any;
 
   routeForm = this.fb.group({
@@ -82,7 +76,9 @@ export class RouteCreateComponent implements OnInit {
     await this.routeService.createRoute(this.route)
       .subscribe(data => console.log(data), error => console.log(error));
 
-    await this.routeService.getRouteByName(this.route.name)
+    await this.delay(500);
+
+    this.routeService.getRouteByName(this.route.name)
       .subscribe(data => {
         console.log(data);
         this.route = data;
@@ -90,35 +86,20 @@ export class RouteCreateComponent implements OnInit {
       }, error => console.log(error));
 
     this.route = new Route();
-    //this.gotoList();
   }
 
   createDestinations(id: number) {
     this.router.navigate(['destinations/create/route', id]);
   }
 
-  /*
-  gotoList() {
-    this.router.navigate(['/routes']);
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
- */
 
   checkWarehouses(group: FormGroup) {
     let id_first_warehouse = group.get('id_first_warehouse').value;
     let id_last_warehouse = group.get('id_last_warehouse').value;
 
     return id_first_warehouse !== id_last_warehouse ? null : { notSame: true }
-  }
-
-  addNewDestination() {
-    this.count++;
-    alert("You clicked button!");
-    this.dest.push({id:0, id_route:0, id_warehouse:0, order: this.count});
-    /*
-    this.destinations.subscribe(elements => {
-      elements.push(this.destination);
-    });
-    */
-
   }
 }
